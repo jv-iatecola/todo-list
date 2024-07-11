7e99c1880e09aad675130da666bcbc57b6824030
jv-iatecola <jv.iatecola10@gmail.com>
2024-07-11 19:59:42 -0300

    added funtional edit button

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const inputElement = document.querySelector("input")
+let inputElement = document.querySelector("input")
 const buttonElement = document.querySelector("button")
 
 let inputResponse = ''
@@ -7,32 +7,49 @@ const todoList = []
 inputElement.addEventListener("input", (event)=>{
     inputResponse = event.target.value
 })  
-    
+
+let selectedItem = null
+
 buttonElement.addEventListener("click", ()=>{
-    todoList.push({
-        value: inputResponse,
-        id: Math.random()
-    })
 
-    const pElement = document.createElement("p")
-    pElement.textContent = inputResponse
+    if (buttonElement.textContent === "add"){
+        const textElement = document.createElement("p")
+        
+        // todoList.push({
+        //     value: inputResponse,
+        //     id: Math.random()   
+        // })
+            
+        textElement.textContent = inputResponse
+        textElement.id = Math.random()
 
-    const editButton = document.createElement("button")
-    editButton.textContent = "Edit"
+        const editButton = document.createElement("button")
+        editButton.textContent = "Edit"
+        
+        const deleteButton = document.createElement("button")
+        deleteButton.textContent = "Delete"
+               
+        editButton.addEventListener("click", ()=>{
+            console.log(textElement.id)
+            buttonElement.textContent = "Confirm"
+            inputElement.value = textElement.textContent
+            selectedItem = textElement.id
+        })
+        
+        deleteButton.addEventListener("click", ()=>{
+            document.body.removeChild(textElement)
+            document.body.removeChild(deleteButton)
+            document.body.removeChild(editButton)
+        })
+        
+        document.body.prepend(textElement, editButton, deleteButton)
+    }else{
+        const textElement = document.getElementById(selectedItem)
+        textElement.textContent = inputResponse
 
-    const deleteButton = document.createElement("button")
-    deleteButton.textContent = "Delete"
-
-    editButton.addEventListener("click", ()=>{
-        console.log("Edit Print!")
-    })
-
-    deleteButton.addEventListener("click", ()=>{
-        document.body.removeChild(pElement)
-        document.body.removeChild(deleteButton)
-        document.body.removeChild(editButton)
-    })
-
-    document.body.prepend(pElement, editButton, deleteButton)
-
+        buttonElement.textContent = "add"
+    }
+    
+    // textElement.textContent = inputResponse
+     
 })
